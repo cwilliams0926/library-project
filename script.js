112ecc817b79e27493330198ac7a51b056fc12ec
cwilliams0926 <coopwilliams03@hotmail.com>
2026-06-03 01:08:03 -0400

    Add displayBooks() function

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function addBookToLibrary(name, author, pageCount, read) {
   const book = new Book(name, author, pageCount, read);
   myLibrary.push(book);
 }
+
+addBookToLibrary("test", "test", 92, true);
+addBookToLibrary("test2", "test2", 102, false);
+addBookToLibrary("test3", "test3", 112, true);
+
+function displayBooks() {
+  const tableBody = document.querySelector("#table-body");
+  myLibrary.forEach((book) => {
+    const tableRow = document.createElement("tr");
+    for (const key in book) {
+      if (Object.hasOwn(book, key)) {
+        const tableCell = document.createElement("td");
+        tableCell.textContent = book[key];
+        tableRow.appendChild(tableCell);
+      }
+    }
+    tableBody.appendChild(tableRow);
+  });
+}
+
+displayBooks();
